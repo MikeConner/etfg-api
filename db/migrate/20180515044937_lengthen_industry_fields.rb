9b147dcd17fa3123c08e4e88d4a770f2e0d0cb8b
Jeff <endymionjkb@gmail.com>
2018-05-15 01:12:03 -0400

    rollback issue

--- db/migrate/20180515044937_lengthen_industry_fields.rb
+++ db/migrate/20180515044937_lengthen_industry_fields.rb
@@ -8,10 +8,10 @@ class LengthenIndustryFields < ActiveRecord::Migration[5.2]
   end
   
   def down
-    change_column :industries, :issuer, :string, :limit => 32
+    change_column :industries, :issuer, :string, :limit => 64
     change_column :industries, :related_index, :string, :limit => 128
     change_column :industries, :name, :string, :limit => 128
-    change_column :industries, :tax_classification, :string, :limit => 32   
+    change_column :industries, :tax_classification, :string, :limit => 64   
     change_column :industries, :avg_volume, :string, :limit => 10
   end
 end
